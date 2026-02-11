8ab6993ad422791e8d7839bc14a0cc089b264b71
Zied Attia <attia.zd@gmail.com>
2026-02-11 21:28:10 +0100

    feat: add real supplier search URLs and priorities

--- lib/suppliers.ts
+++ lib/suppliers.ts
@@ -1,60 +1,58 @@
 export type Supplier = {
   name: string
-  searchUrl: string
+  baseUrl: string
   priority: number
   active: boolean
-  favorite?: boolean
 }
 
-export const SUPPLIERS: Supplier[] = [
+export const suppliers: Supplier[] = [
   {
     name: 'Sodimas',
-    searchUrl: 'https://my.sodimas.com/fr/recherche?searchstring=',
+    baseUrl: 'https://my.sodimas.com/fr/recherche?searchstring=',
     priority: 100,
     active: true,
-    favorite: true,
   },
   {
     name: 'Elvacenter',
-    searchUrl: 'https://shop.elvacenter.com/#/dfclassic/query=',
+    baseUrl: 'https://shop.elvacenter.com/#/dfclassic/query=',
     priority: 90,
     active: true,
   },
   {
     name: 'Hauer',
-    searchUrl: 'https://www.elevatorshop.de/fr#/dfclassic/query=',
-    priority: 85,
-    active: true,
-  },
-  {
-    name: 'Kone',
-    searchUrl: 'https://parts.kone.com/#/!1@&searchTerm=',
+    baseUrl: 'https://www.elevatorshop.de/fr#/dfclassic/query=',
     priority: 80,
     active: true,
   },
   {
-    name: 'MGTI',
-    searchUrl:
-      'https://www.mgti.fr/PBSearch.asp?ActionID=1&CCode=2&ShowSMImg=1&SearchText=',
-    priority: 75,
-    active: true,
-  },
-  {
-    name: 'Sodica',
-    searchUrl: 'https://www.sodica.fr/fr/search?SearchTerm=',
+    name: 'Kone',
+    baseUrl: 'https://parts.kone.com/#/!1@&searchTerm=',
     priority: 70,
     active: true,
   },
   {
+    name: 'MGTI',
+    baseUrl:
+      'https://www.mgti.fr/PBSearch.asp?ActionID=1&CCode=2&ShowSMImg=1&SearchText=',
+    priority: 60,
+    active: true,
+  },
+  {
+    name: 'Sodica',
+    baseUrl: 'https://www.sodica.fr/fr/search?SearchTerm=',
+    priority: 50,
+    active: true,
+  },
+  {
     name: 'RS',
-    searchUrl: 'https://befr.rs-online.com/web/c/?searchTerm=',
-    priority: 65,
+    baseUrl: 'https://befr.rs-online.com/web/c/?searchTerm=',
+    priority: 40,
     active: true,
   },
   {
     name: 'Cebeo',
-    searchUrl: 'https://www.cebeo.be/catalog/fr-be/search/',
-    priority: 60,
+    baseUrl: 'https://www.cebeo.be/catalog/fr-be/search/',
+    priority: 30,
     active: true,
   },
 ]
