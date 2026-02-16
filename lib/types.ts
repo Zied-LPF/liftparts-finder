bb73cdff6fa8d59832170509d5045026b0511390
Zied Attia <attia.zd@gmail.com>
2026-02-16 17:56:17 +0100

    feat(types): add SupplierResult type for connecteurs

---
+++ lib/types.ts
@@ -0,0 +1,11 @@
+// lib/types.ts
+export type SupplierResult = {
+  supplier: string
+  title: string
+  reference?: string
+  link: string
+  price?: number
+  brand?: string
+  source?: string
+  score?: number
+}
